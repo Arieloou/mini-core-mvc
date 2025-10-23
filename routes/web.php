e366dc21d25d386447088176d6e321b7947f1f80
Arieloou <ariel.anchapaxi@gmail.com>
2025-10-22 22:11:54 -0500

    Updated web routes for HTTP requests

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use App\Http\Controllers\ComissionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ComissionController::class, 'index'])->name('commission.index');
-Route::post('/calculate', [ComissionController::class, 'calculate'])->name('commission.calculate');
+Route::post('/', [ComissionController::class, 'calculate'])->name('commission.calculate');
